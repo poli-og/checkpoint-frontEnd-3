542ee84d65f34bb3e9c48297eb3d94ec69956ac4
poli-og <poliana.oliveira.gomes@gmail.com>
2022-04-09 01:27:53 -0300

    first bugs fixed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -33,7 +33,7 @@ function Footer() {
                 <p className="copywright">
                     <h3>Feito com 💜 e <FaReact size={20} /> por Poliana Gomes </h3>
                 </p>
-                <svg className="btn-top" id="scroll-top" onClick={topFunction} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clipRule="evenodd" /></svg>
+                <button className="scroll-top" id="scroll-top" onClick={topFunction}><svg  className="btn-top" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clipRule="evenodd" /></svg></button>
         </footer>
     );
 }
